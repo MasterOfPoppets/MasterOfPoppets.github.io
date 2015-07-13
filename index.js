4915b62381bb2004e0cfc52294903765b8a8ce0b
Gareth Hughes <gurrkin@googlemail.com>
2015-07-13 22:28:37 +0100

    [Update] More metalsmith and handlebars learning

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var Metalsmith = require('metalsmith'),
 	drafts = require('metalsmith-drafts'),
 	markdown = require('metalsmith-markdown'),
+	excerpts = require('metalsmith-excerpts'),
 	templates = require('metalsmith-templates'),
 	collections = require('metalsmith-collections'),
 	permalinks = require('metalsmith-permalinks'),
-	serve = require('metalsmith-serve')
+	serve = require('metalsmith-serve'),
+	watch = require('metalsmith-watch')
 
 Metalsmith(__dirname)
 	.source('./src')
 	.destination('./build')
 	.use(drafts())
 	.use(markdown())
+	.use(excerpts())
 	.use(permalinks({
 		pattern: ':title'
 	}))
@@ -19,12 +22,28 @@ Metalsmith(__dirname)
 			pattern: '*/*.html',
 			sortBy: 'date',
 			reverse: true
+		},
+		homePosts: {
+			pattern: '*/*.html',
+			sortBy: 'date',
+			reverse: true,
+			limit: 3
 		}
 	}))
-	.use(templates('handlebars'))
+	.use(templates({
+		engine: 'handlebars',
+		directory: 'templates',
+		partials: {
+			header: 'header',
+			footer: 'footer'
+		}
+	}))
 	.use(serve({
 		port: 8080,
 		verbose: true
+	}))
+	.use(watch({
+		pattern: '**/*'
 	}))
 	.build(function (err) {
 		if (err) throw err
