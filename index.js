8046e67c54a065ed9e827d4a8c82d7a2654a30a7
Gareth Hughes <gurrkin@googlemail.com>
2015-07-21 22:04:00 +0100

    [Update] Pure.css styling

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var Metalsmith = require('metalsmith'),
     Handlebars = require('handlebars'),
     metalsmithIf = require('metalsmith-if'),
 	drafts = require('metalsmith-drafts'),
+    less = require('metalsmith-less'),
 	markdown = require('metalsmith-markdown'),
 	excerpts = require('metalsmith-excerpts'),
 	templates = require('metalsmith-templates'),
 	collections = require('metalsmith-collections'),
 	permalinks = require('metalsmith-permalinks'),
+    ignore = require('metalsmith-ignore'),
 	serve = require('metalsmith-serve'),
 	watch = require('metalsmith-watch'),
 	config = require('./config')(process.argv)
@@ -19,6 +21,7 @@ Metalsmith(__dirname)
 	.source('./src')
 	.destination('./build')
 	.use(drafts())
+    .use(less())
 	.use(markdown())
 	.use(excerpts())
 	.use(permalinks({
@@ -45,6 +48,10 @@ Metalsmith(__dirname)
 			footer: 'partials/footer'
 		}
 	}))
+    .use(ignore([
+        'styles/*',
+        '!styles/main.css'
+    ]))
     .use(metalsmithIf(config.isDev, serve({
         port: 8080,
         verbose: true
